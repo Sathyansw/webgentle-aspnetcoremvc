c39e21fbbf53c5ef7d8e5695d6aa0a67a8b04396
Sathyan <sathyansw.it@gmail.com>
2025-06-14 12:59:36 +0530

    Added mvc to application

--- Webgentle.BookStore/Webgentle.BookStore/Program.cs
+++ Webgentle.BookStore/Webgentle.BookStore/Program.cs
@@ -1,6 +1,20 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+
+
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapDefaultControllerRoute();
+});
+
+
 
 app.Run();
